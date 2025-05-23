558351a16c20dcc36efc8205350ab237054d6f7b
Mohamed Boukouch <boukouchmohamed7@gmail.com>
2025-05-24 00:10:01 +0100

    rrr

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import GameStore from './GameStore';
 import GameDetail from './GameDetail';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<GameStore />} />
-        <Route path="/game/:id" element={<GameDetail />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<GameStore />} />
+          <Route path="/game/:id" element={<GameDetail />} />
+        </Routes>
+      </Router>
+      <Analytics />
+    </>
   );
 }
 
